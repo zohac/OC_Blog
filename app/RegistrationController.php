ab1941f0e81f467de1f6050e383ecfb8c193af1d
zohac <fenrir0680@gmail.com>
2018-01-20 17:54:33 +0100

    Bug Fix! 1. test if a user is banned. 2. test if a user already exists.

--- app/RegistrationController.php
+++ app/RegistrationController.php
@@ -13,7 +13,7 @@ class RegistrationController extends Controller
     public function executeRegistration()
     {
         if (!empty($_POST)) {
-            if ($_POST['password'] === $_POST['password2']) {
+            if ($this->samePassword($_POST['password'], $_POST['password2'])) {
                 $Validator = Container::getValidator();
 
                 $Validator->required('pseudo', 'text');
@@ -25,27 +25,57 @@ class RegistrationController extends Controller
                     $params = $Validator->getParams();
                     $encryptedPassword = Container::getEncryption()->hash($params);
                     $manager = $this->getManager();
-                    $answer = $manager->Registration($params['pseudo'], $params['email'], $encryptedPassword);
 
-                    if ($answer === false) {
-                        $this->setParams([
-                            'errorRegistration'
-                            => 'Une erreur est survenu lors de votre inscription, veuillez réessayer!'
-                        ]);
-                    } else {
-                        $this->user->setAuthenticated();
+                    if (!$this->userExist($params['email']) OR !$this->userBanned($params['email'])) {
+                        $answer = $manager->Registration($params['pseudo'], $params['email'], $encryptedPassword);
 
-                        $reponse = Container::getHTTPResponse();
-                        $reponse->setStatus(301);
-                        $reponse->redirection('/admin');
+                        if ($answer === false) {
+                            $this->setParams([
+                                'errorRegistration'
+                                => 'Une erreur est survenu lors de votre inscription, veuillez réessayer!'
+                            ]);
+                        } else {
+                            $reponse = Container::getHTTPResponse();
+                            $reponse->setStatus(301);
+                            $reponse->redirection('/login');
+                        }
                     }
                 }
                 $this->setParams($Validator->getParams());
-            } else {
-                $this->setParams(['errorRegistration' => 'Les deux mots de passe ne sont pas identique!']);
             }
         }
         $this->getView();
         $this->send();
+    }
+
+    public function userExist(string $email):bool
+    {
+        $manager = $this->getManager();
+        if ($manager->userExist($email)) {
+            $this->setParams(['errorRegistration' => 'L\'utilisateur existe déjà!']);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function userBanned(string $email):bool
+    {
+        $manager = $this->getManager();
+        if ($manager->userBanned($email)) {
+            $this->setParams(['errorRegistration' => 'L\'adresse email a été bannie!']);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function samePassword(string $password1,string $password2):bool
+    {
+        if ($password1 != $password2) {
+            $this->setParams(['errorRegistration' => 'Les deux mots de passe ne sont pas identique!']);
+            return false;
+        }
+        return true;
     }
 }
