81819807fab9ec62ac5084e85e305c3988ad9634
zohac <fenrir0680@gmail.com>
2018-01-20 18:04:44 +0100

    Check if the user is banned!

--- app/model/LoginManager.php
+++ app/model/LoginManager.php
@@ -11,14 +11,20 @@ class LoginManager extends Manager
 
     public function getUser(string $email, string $password)
     {
-        $sql = "SELECT * FROM blog.user WHERE user.email = :email AND user.password = :password";
+        $sql = "
+        SELECT *
+        FROM blog.user
+        WHERE user.email = :email
+            AND user.password = :password
+            AND user.status != 'banned'
+        ";
 
         $requete = $this->DB->prepare($sql);
         $requete->bindValue(':email', $email, \PDO::PARAM_STR);
         $requete->bindValue(':password', $password, \PDO::PARAM_STR);
         $requete->execute();
         $userInfo = $requete->fetch();
-        
+
         return $userInfo;
     }
 }
